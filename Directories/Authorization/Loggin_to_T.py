4e820811f5d1e431fad9aaa3f0ea26b293d54abd
Ilya Erlingas <Aligruit@gmail.com>
2020-03-12 15:31:41 +0300

    +#03correction

--- Directories/Authorization/Loggin_to_T.py
+++ Directories/Authorization/Loggin_to_T.py
@@ -7,7 +7,7 @@ class LoginByUrl:
         self.forward_text = forward_text
         self.bot_username = bot_username
         self.request_write_access = request_write_access
-        self.token = '837019930:AAFCyNv-WlgKQ-48dptM4rQSZ14_WxEoq5A'
+        self.token = ''
 
         self._id_attrs = (self.url,)
 
---
+++ Directories/Tests/test_auth.py
@@ -0,0 +1,4 @@
+import unittest
+from directories.translation_YA import translate_my_word
+
+
