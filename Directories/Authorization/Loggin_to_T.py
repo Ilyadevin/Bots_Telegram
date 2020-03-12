8ebd55597c5e5ff59027f3fd9c8e078eb085015c
Ilya Erlingas <Aligruit@gmail.com>
2020-03-12 14:35:06 +0300

    +OOP LogIn

--- Directories/Authorization/Loggin_to_T.py
+++ Directories/Authorization/Loggin_to_T.py
@@ -1,2 +1,18 @@
-import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+from telegram import TelegramObject
+
+
+class LoginByUrl:
+    def __init__(self, url, forward_text=None, bot_username=None, request_write_access=None):
+        self.url = url
+        self.forward_text = forward_text
+        self.bot_username = bot_username
+        self.request_write_access = request_write_access
+        self.token = '837019930:AAFCyNv-WlgKQ-48dptM4rQSZ14_WxEoq5A'
+
+        self._id_attrs = (self.url,)
+
+    def lets_log(self):
+        start_log = f'{self.url}<{self.token}/getMe'
+
+
+Login = LoginByUrl('https://api.telegram.org/bot')
