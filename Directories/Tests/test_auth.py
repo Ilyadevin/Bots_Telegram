a01b163e5f5f0f479cabb86cc4fff05492a7a685
Ilya Erlingas <Aligruit@gmail.com>
2020-03-28 00:14:09 +0300

    +test

--- Directories/Tests/test_auth.py
+++ Directories/Tests/test_auth.py
@@ -1,4 +1,26 @@
+from __future__ import absolute_import
+
 import unittest
-from directories.translation_YA import translate_my_word
+from Directories.Authorization.Loggin_to_T import *
+
+import unittest
+
+from ptbtest import (BadBotException, BadCallbackQueryException,
+                     BadUserException, BadMessageException)
+from ptbtest import (CallbackQueryGenerator, MessageGenerator, Mockbot,
+                     UserGenerator)
+from telegram import (CallbackQuery, Message, Update, User)
 
 
+class Test_case(unittest.TestCase):
+    def setUp(self) -> None:
+        self.example_callback=CallbackQueryGenerator()
+        self.example_set = {'first_message': 'start'}
+
+    # test for answer
+    def test_answer(self):
+        with self.assertRaisesRegexp(BadCallbackQueryException, 'message and inline_message_id'):
+            self.example_callback.get_callback_query()
+        with self.assertRaisesRegexp(BadCallbackQueryException,
+                                     "message and inline_message_id"):
+            self.example_callback.get_callback_query(message=True, inline_message_id=True)
