ada31f736e96b9b07c3fb984aec9ecb5aabcc231
Ilya Erlingas <Aligruit@gmail.com>
2020-06-06 22:30:54 +0300

    upgrade settings files to json format

--- conf_packages/settings_for_all.py
+++ conf_packages/settings_for_all.py
@@ -1,48 +1,39 @@
-import configparser
+import json
 import telebot
 from googletrans import Translator
 import time
 from conf_packages.keyboard_buttons import *
 import os
 import wikipediaapi
+
 wiki_wiki = wikipediaapi.Wikipedia('en')
 statusd = 'close'
 statusw = 'close'
 mode = 0
-list_of_log_data = []
 lang = 'ru'
 
 
-def yandex_c():
-    config_ya = configparser.ConfigParser()
-    config_ya.read('config_YA.ini')
-    config_yandex = config_ya['api_keys']['yandex']
-    return config_yandex
+class TokensSettings:
+    def __init__(self):
+        with open('config.json', 'r', encoding='utf8') as json_file:
+            self.reader = json.load(json_file)
+
+    def telegram_main_token(self):
+        for token in self.reader:
+            telegram_token = token['telegram_api']
+            return telegram_token
+
+    def yandex_token(self):
+        for token in self.reader:
+            yandex_token = token['yandex_api']
+            return yandex_token
+
+    def google_settings(self):  # This is the test format
+        for settings in self.reader:
+            service_urls = [settings['google_settings'][0],settings['google_settings'][1]]
+            return service_urls
 
 
-def meeting_c():
-    config_m = configparser.ConfigParser()
-    config_m.read('config_M.ini')
-    config_meeting = config_m['api_keys']['TOKEN']
-    return config_meeting
-
-
-bot_meeting = telebot.TeleBot(meeting_c())
-
-
-def helper_c():
-    config_t = configparser.ConfigParser()
-    config_t.read('config.ini')
-    config_telegram = config_t['api_keys']['helper_bot']
-    return config_telegram
-
-
-bot_helper = telebot.TeleBot(helper_c())
-
-
-def google_settings():
-    google_translation = Translator(service_urls=[
-        'translate.google.com',
-        'translate.google.co.kr',
-    ])
-    return google_translation
+bot_helper = TokensSettings().telegram_main_token()
+yandex_token_settings = TokensSettings().yandex_token()
+google_settings = Translator(TokensSettings().google_settings())
