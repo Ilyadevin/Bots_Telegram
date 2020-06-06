2f64ed0cd6d5528b581d0675ef5dbc6614c2ad80
Ilya Erlingas <Aligruit@gmail.com>
2020-06-06 22:24:47 +0300

    deleting data with meeting bot

--- bots/bot_meeting_app/meeting_bot.py
+++
@@ -1,8 +0,0 @@
-from conf_packages.settings_for_all import *
-
-
-@bot_meeting.message_handler(commands=['start'])
-def start_message(message):
-    bot_meeting.send_message(message.chat.id, 'Hello there! Lets start?',
-                             reply_markup=keyboard_settings_meeting()
-                             )
